963ccb7f44cb1d2026e1b388ab589f6d62286e8d
Sanntidshallen <>
2025-10-28 12:10:22 +0100

    Added Motor_driver files

--- node2/include/pwm.h
+++ node2/include/pwm.h
@@ -3,19 +3,11 @@
 
 #include "sam.h"
 
-
-#define SERVO_PIN PIO_PB13
-#define SERVO_PORT PORTB
-#define FREQ 50
-
 void pwm_pin_setup();
 void pwm_init(uint8_t channel);
 void pwm_enable(uint8_t channel);
 void pwm_disable(uint8_t channel);
 void pwm_set_duty_us(uint8_t channel, uint32_t us);
 void servo_init();
-
-//uint32_t can_to_pwm(int joystick_input);
-
 
 #endif
