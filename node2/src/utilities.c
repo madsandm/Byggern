7a44d511302a46d3f3ecc8728b77c8cf6d055c54
Sanntidshallen <>
2025-10-28 16:14:10 +0100

    added include

--- node2/src/utilities.c
+++ node2/src/utilities.c
@@ -2,6 +2,7 @@
 #include "can_controller.h"
 #include "pwm.h"
 #include "controller.h"
+#include "motor_driver.h"
 
 uint32_t can_joystick_to_us(){
     volatile int x;
@@ -13,7 +14,7 @@ uint32_t can_joystick_to_us(){
             x = -(msg_rx.data[0]-130);
             y = ((msg_rx.data[1]) * 47 + 9000)/10;
             b = msg_rx.data[2];
-            motorController_setTarget(x);
+            //motorController_setTarget(x);
             motor_driver_set_vel(x);
             pwm_set_duty_us(1,y);
             printf("%d %d %d\n", x,y,b);
